887c5ccca517203fbc74c8dd8f48a1541a8100ab
Nguyen Tri Nhan <trinhan194@gmail.com>
2022-10-23 15:20:55 +0700

    Add mock 100 products

--- src/constants/product.ts
+++ src/constants/product.ts
@@ -5,3 +5,17 @@ export const EMPTY_PRODUCT: Product = {
   name: '',
   price: 0,
 };
+
+export const MOCK_100_PRODUCTS: Product[] = Array.apply(0, Array(100)).map(
+  (_, index) =>
+    ({
+      id: index,
+      name: 'Product name #' + index,
+      price: 100,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to ",
+      technicalInformation:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ",
+      rating: 4,
+    } as Product)
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,9 @@ import { NextPageWithLayout, Product } from '@/types';
 import { BoxIcon, ProductModal } from '@/components';
 import clsx from 'clsx';
 import useStore from '@/store';
-import { HomePageSectionType } from '@/constants';
-import { EMPTY_PRODUCT } from '@/constants/product';
+import { EMPTY_PRODUCT, HomePageSectionType, MOCK_100_PRODUCTS } from '@/constants';
 import ProductCard from '@/components/ProductCard';
 import { notifyUpcoming } from '@/utils';
-
-const listItems = Array.apply(0, Array(15)).map(
-  (_, index) =>
-    ({
-      id: index,
-      name: 'Product name #' + index,
-      price: 100,
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to ",
-      technicalInformation:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ",
-      rating: 4,
-    } as Product)
-);
 
 const TitleDictionary: Record<HomePageSectionType, string> = {
   [HomePageSectionType.HOME]: 'Home',
@@ -110,7 +95,7 @@ const HomePage: NextPageWithLayout = () => {
 
   const productList = (
     <div className="mt-6 grid grid-cols-4 xl:grid-cols-5 gap-x-5 gap-y-8">
-      {listItems.map((product) => (
+      {MOCK_100_PRODUCTS.map((product) => (
         <ProductCard key={product.name + product.id} product={product} onClick={onProductCardClick} />
       ))}
     </div>
